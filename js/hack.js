6d913efa0cb31d3da4e0be60d7692b58e20f313a
asvnpr <vega360@gmail.com>
2016-08-18 14:03:48 -0400

    finished silly js scripts to show/hide elements and brute force a fake 4 digit password. for first aecc presentation

--- js/hack.js
+++ js/hack.js
@@ -1,3 +1,13 @@
-function hello (){
-    console.log('hey');
+function show() {
+    document.getElementById("start").style.display = "block";
+    document.getElementById("click").style.display = "none";
+}
+
+function check() {
+    var pin = document.getElementById("pin").value;
+    console.log(pin);
+    console.log(typeof(pin));
+    if (pin === "4030") {
+        document.getElementById("hack").style.display = "block";
+    }
 }
